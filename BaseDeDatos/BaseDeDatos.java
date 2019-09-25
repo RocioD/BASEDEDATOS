0a54bc370a98fae330dedda6fe75d1074692c425
Rocío Díaz San Martín <rocio.diaz-sm@hotmail.com>
2019-09-25 15:50:21 -0300

    Primer commit

---
+++ BaseDeDatos/BaseDeDatos.java
@@ -0,0 +1,21 @@
+
+public class BaseDeDatos {
+
+    public void agregar(Dato d) {
+        System.out.println("Agrega un objeto a la base de datos emulada");
+    }
+
+    public void eliminar(Dato d) {
+        System.out.println("Elimina un objeto de la base de datos emulada.");
+    }
+
+    public void listar() {
+        System.out.println("Retorna una lista con todos los objetos de la base de datos emulada.");
+    }
+
+    public void modificar(Dato d) {
+        System.out.println("Modifica un objeto de la base de datos emulada.");
+    }
+
+
+}
